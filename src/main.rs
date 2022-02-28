1d9f69c920bf329702c89aca987b9f1ff0c64641
Ed Sweeney <ed@onextent.com>
2022-02-27 21:53:58 -0800

    init

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,23 @@
+use clap::{App, Arg};
+
 fn main() {
-    println!("{:?}", std::env::args());
+    let matches = App::new("echor")
+        .version("0.1.0")
+        .author("Ed Sweeney <ed@onextent.com>")
+        .about("Rust echor")
+        .arg(
+            Arg::with_name("text")
+                .value_name("TEXT")
+                .help("Input text")
+                .required(true)
+                .min_values(1),
+        )
+        .arg(
+            Arg::with_name("omit newline")
+                .short("n")
+                .help("Input text")
+                .takes_value(false),
+        )
+        .get_matches();
+    println!("{:#?}", matches);
 }
